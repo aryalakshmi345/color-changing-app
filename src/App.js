c32cef268fed03aa634341877099de91200d57ce
aryalakshmi345 <lekshmiarya113@gmail.com>
2023-10-27 19:32:27 +0530

    background color changing app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
      <div className='d-flex justify-content-center align-items-center w-100' style={{height:'100vh'}}>
         <div className="border shadow rounded d-flex justify-content-center align-items-center flex-column w-75" style={{height:'80vh' , backgroundColor: bgColor}}>
             <h2  className='text-light mb-4'>Background Color Change</h2>
-            <div className="w-75 d-flex justify-content-evenly align-items-center">
+            <div className="w-75 d-flex flex-wrap justify-content-evenly align-items-center">
               <button className='btn btn-dark border-light' style={{width:'100px'}} onClick={()=>setBgColor('#212529')}>BLACK</button>
               <button className='btn btn-warning border-light text-light' style={{width:'100px'}} onClick={()=>setBgColor('#ffc107')}>YELOW</button>
               <button className='btn btn-primary border-light' style={{width:'100px'}} onClick={()=>setBgColor('#0d6efd')}>BLUE</button>
